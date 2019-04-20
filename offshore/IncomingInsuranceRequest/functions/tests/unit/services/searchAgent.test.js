9ecd863580be74ac74522ef3a327e8276fad4e21
dexterchan <dexterchan@gmail.com>
2019-04-20 21:37:54 +0800

    timeout fix

--- offshore/IncomingInsuranceRequest/functions/tests/unit/services/searchAgent.test.js
+++ offshore/IncomingInsuranceRequest/functions/tests/unit/services/searchAgent.test.js
@@ -1,6 +1,8 @@
 require("../../firebaseTestObj");
 const searchIncomingTicket = require("../../../services/searchAgent");
 const uuidv4 = require("uuid/v4");
+
+const ALLOW_TIMEOUT = 1000000;
 const {
   Gender,
   InsuranceType,
@@ -24,55 +26,79 @@ describe("test agent", () => {
     };
   });
 
-  it("should return error if given invalid ticket", async () => {
-    delete ticket.amount;
-    delete ticket.insurance_type;
-    try {
-      await searchIncomingTicket(ticket);
-      throw new Error("insurance_type is missing");
-    } catch (ex) {
-      //console.log(ex.message);
-      expect(ex.message).toMatch(/"insurance_type" is required/);
-    }
-  });
+  it(
+    "should return error if given invalid ticket",
+    async () => {
+      delete ticket.amount;
+      delete ticket.insurance_type;
+      try {
+        await searchIncomingTicket(ticket);
+        throw new Error("insurance_type is missing");
+      } catch (ex) {
+        //console.log(ex.message);
+        expect(ex.message).toMatch(/"insurance_type" is required/);
+      }
+    },
+    ALLOW_TIMEOUT
+  );
 
-  it("should return query with valid gender + insurance type", async () => {
-    ticket.agentGender = Gender.FEMALE;
-    const agents = await searchIncomingTicket(ticket);
-    expect(agents.length).toBe(0);
-  });
+  it(
+    "should return query with valid gender + insurance type",
+    async () => {
+      ticket.agentGender = Gender.FEMALE;
+      const agents = await searchIncomingTicket(ticket);
+      expect(agents.length).toBe(0);
+    },
+    ALLOW_TIMEOUT
+  );
 
-  it("should return nothing with valid gender + insurance type + lang", async () => {
-    ticket.agentGender = Gender.MALE;
-    ticket.agentAgeGrp = AgeGrp.E25_30;
-    ticket.language = Language.English;
-    const agents = await searchIncomingTicket(ticket);
-    //console.log(agents);
-    expect(agents.length).toBe(0);
-  });
+  it(
+    "should return nothing with valid gender + insurance type + lang",
+    async () => {
+      ticket.agentGender = Gender.MALE;
+      ticket.agentAgeGrp = AgeGrp.E25_30;
+      ticket.language = Language.English;
+      const agents = await searchIncomingTicket(ticket);
+      //console.log(agents);
+      expect(agents.length).toBe(0);
+    },
+    ALLOW_TIMEOUT
+  );
 
-  it("should return nothing with valid gender + insurance type + lang +exp", async () => {
-    ticket.agentGender = Gender.MALE;
-    ticket.agentAgeGrp = AgeGrp.E25_30;
-    ticket.language = Language.ANY;
-    ticket.agentExp = AgentExp.E2Y_5Y;
-    const agents = await searchIncomingTicket(ticket);
-    //console.log(agents);
-    expect(agents.length).toBe(0);
-  });
-  it("should return query with valid gender + insurance type + lang +exp", async () => {
-    ticket.agentGender = Gender.MALE;
-    ticket.agentAgeGrp = AgeGrp.E25_30;
-    ticket.language = Language.ANY;
-    ticket.agentExp = AgentExp.E10Y;
-    const agents = await searchIncomingTicket(ticket);
-    //console.log(agents);
-    expect(agents.length).toBe(1);
-  });
-  it("should return query with valid gender + insurance type + lang", async () => {
-    ticket.agentGender = Gender.MALE;
-    ticket.agentAgeGrp = AgeGrp.E25_30;
-    const agents = await searchIncomingTicket(ticket);
-    expect(agents.length).toBe(1);
-  });
+  it(
+    "should return nothing with valid gender + insurance type + lang +exp",
+    async () => {
+      ticket.agentGender = Gender.MALE;
+      ticket.agentAgeGrp = AgeGrp.E25_30;
+      ticket.language = Language.ANY;
+      ticket.agentExp = AgentExp.E2Y_5Y;
+      const agents = await searchIncomingTicket(ticket);
+      //console.log(agents);
+      expect(agents.length).toBe(0);
+    },
+    ALLOW_TIMEOUT
+  );
+  it(
+    "should return query with valid gender + insurance type + lang +exp",
+    async () => {
+      ticket.agentGender = Gender.MALE;
+      ticket.agentAgeGrp = AgeGrp.E25_30;
+      ticket.language = Language.ANY;
+      ticket.agentExp = AgentExp.E10Y;
+      const agents = await searchIncomingTicket(ticket);
+      //console.log(agents);
+      expect(agents.length).toBe(1);
+    },
+    ALLOW_TIMEOUT
+  );
+  it(
+    "should return query with valid gender + insurance type + lang",
+    async () => {
+      ticket.agentGender = Gender.MALE;
+      ticket.agentAgeGrp = AgeGrp.E25_30;
+      const agents = await searchIncomingTicket(ticket);
+      expect(agents.length).toBe(1);
+    },
+    ALLOW_TIMEOUT
+  );
 });
